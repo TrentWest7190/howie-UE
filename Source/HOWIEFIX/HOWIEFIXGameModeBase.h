45d49ddbd74377c540595a36cdcdcc5b343e2662
Trent West <trentwest7190@gmail.com>
2023-12-26 19:37:14 -0600

    init

---
+++ Source/HOWIEFIX/HOWIEFIXGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HOWIEFIXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOWIEFIX_API AHOWIEFIXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
